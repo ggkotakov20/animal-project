4566649a9da5df15fc8d307e8cca38a68449397e
Georgi Kotakov <ggkotakov20@codingburgas.bg>
2025-04-16 20:09:05 +0300

    Add welcome api

---
+++ BackendCore/src/main/java/com/backendcore/backendcore/Welcome.java
@@ -0,0 +1,17 @@
+package com.backendcore.backendcore;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("")
+public class Welcome {
+    @GetMapping("/online")
+    public ResponseEntity<String> getAllAccountCategories() {
+        return ResponseEntity.ok(null);
+    }
+}
